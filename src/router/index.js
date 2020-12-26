e36bf8b3511f692eff8e8ec68d79f39530c109c8
Rico Putra <rico.putra@outlook.co.id>
2020-12-26 23:17:59 +0700

    Add bottom tab menu

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import Home from '../pages/Home';
+import Splash from '../pages/Splash';
+import Pesanan from '../pages/Pesanan';
+import Akun from '../pages/Akun';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+	return (
+		<Tab.Navigator>
+			<Tab.Screen name="Home" component={ Home } />
+			<Tab.Screen name="Pesanan" component={ Pesanan } />
+			<Tab.Screen name="Akun" component={ Akun } />
+		</Tab.Navigator>
+	)
+}
 
 const Router = () => {
     return (
-        <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} />
+        <Stack.Navigator initialRouteName="MainApp">
+            <Stack.Screen name="Splash" component={ Splash } options={{ headerShown: false }}/>
+            <Stack.Screen name="MainApp" component={ MainApp } options={{ headerShown: false }}/>
         </Stack.Navigator>
     )
 }
